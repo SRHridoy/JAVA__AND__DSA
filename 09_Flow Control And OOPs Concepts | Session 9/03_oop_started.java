f9e5bfb26243ff1e7fd107a9db4895ac88b95500
SRHridoy <sohanurrahmanhridoy007@gmail.com>
2023-08-18 21:51:40 +0600

    Upadating

---
+++ 09_Flow Control And OOPs Concepts | Session 9/03_oop_started.java
@@ -0,0 +1,11 @@
+class Person{
+
+    public static void main(String[] args) {
+        int x = 10;
+        read();
+    }
+
+    public static void read() {
+        System.out.println("This is another function!");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# JAVA__AND__DSA
+> # JAVA + DSA
+
+> ## OOP: 
+```
+Piller of OOP:
+    1.Encapsulation = Data Hiding + Abstruction. 
+    2.Inheritance.
+    3.Polymorphism.
+```
+
